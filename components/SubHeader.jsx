d91d0d118763a09850b1024118445d618aa6d933
ATsuddle <ahmad.suddle@lintelfs.com>
2022-10-31 18:22:46 +0500

    adding logo

--- components/SubHeader.jsx
+++ components/SubHeader.jsx
@@ -14,7 +14,7 @@ export default function SubHeader() {
 					<Row>
 						{[1, 2, 3, 4, 5].map((item) => (
 							<Col key={item}>
-								<Image alt='log' src='/logo.jpg' width={100} height={100} />
+								<Image alt='log' src='/logo.JPG' width={100} height={100} />
 							</Col>
 						))}
 					</Row>
