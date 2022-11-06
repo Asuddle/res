931f5c7874c1281c51e6d7354c9be4b5dca1eab2
ATsuddle <ahmad.suddle@lintelfs.com>
2022-11-06 15:33:07 +0500

    about us page

--- components/About/CEOMessage.jsx
+++ components/About/CEOMessage.jsx
@@ -1,10 +1,51 @@
+import { Col, Row } from 'reactstrap';
+
 import ContainerComponent from '../container';
+import Image from 'next/image';
 import styles from '../../styles/About.module.scss';
+
 export default function CEOMessage() {
 	return (
 		<section className={styles.ceoMessageWrapper}>
 			<ContainerComponent>
-				<div className={styles.ceoMessage}></div>
+				<Row className={styles.ceoMessage}>
+					<Col md={4} className={styles.dataPortion}>
+						<p className={styles.ceoName}>Name Here</p>
+						<p>Designation Here</p>
+						<p className={styles.ceoWhoWeAre}>
+							Who we are? <br />
+							Creative <br />
+							visionaries...
+						</p>
+					</Col>
+					<Col md={8} className={styles.imagePortion}>
+						<Image
+							src='/ceo.jpg'
+							alt='ceo'
+							// width='100%'
+							// height='100%'
+							layout='fill'
+							// layout='responsive'
+						/>
+						<div className={styles.ceoMessage}>
+							<p className={styles.heading}>Message from CEO</p>
+							<p className={styles.description}>
+								Lorem ipsum dolor sit amet, conse ctetuer adipiscing elit, sed
+								diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+								aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+								nostrud exerci tation ullam corper suscipit lobortis nisl ut
+								aliquip ex ea commodo consequat.
+								<br />
+								<br />
+								Lorem ipsum dolor sit amet, conse ctetuer adipiscing elit, sed
+								diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+								aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+								nostrud exerci tation ullam corper suscipit lobortis nisl ut
+								aliquip ex ea commodo consequat.
+							</p>
+						</div>
+					</Col>
+				</Row>
 			</ContainerComponent>
 		</section>
 	);
--- components/About/FounderMessage.jsx
+++ components/About/FounderMessage.jsx
@@ -1,16 +1,45 @@
 import { Col, Row } from 'reactstrap';
 
 import ContainerComponent from '../container';
+import Image from 'next/image';
 import styles from '../../styles/About.module.scss';
 
 export default function FounderMessage() {
 	return (
 		<section className={styles.founderMessageWrapper}>
-			<ContainerComponent>
+			<ContainerComponent container='smallContainer'>
 				<div className={styles.founderMessage}>
 					<Row className={styles.internalFounderRow}>
-						<Col md={6}></Col>
-						<Col md={6}></Col>
+						<Col md={6} className={styles.founderImage}>
+							<Image
+								src='/founder.jpg'
+								width={400}
+								height={400}
+								style={{ borderRadius: '14px' }}
+								alt='founder'
+							/>
+							<div className={styles.socialMedia}></div>
+						</Col>
+						<Col md={6}>
+							<p className={styles.founderHeading}>Zuyyana Zaidi</p>
+							<p>Zes Founder</p>
+							<p className={styles.founderMessageHeading}>
+								Message from founder
+							</p>
+							<p className={styles.description} style={{ paddingRight: '20%' }}>
+								Lorem ipsum dolor sit amet, conse ctetuer adipiscing elit, sed
+								diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+								aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+								nostrud exerci tation ullam corper suscipit lobortis nisl ut
+								aliquip ex ea commodo consequat. <br />
+								<br />
+								Lorem ipsum dolor sit amet, conse ctetuer adipiscing elit, sed
+								diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+								aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+								nostrud exerci tation ullam corper suscipit lobortis nisl ut
+								aliquip ex ea commodo consequat.
+							</p>
+						</Col>
 					</Row>
 				</div>
 			</ContainerComponent>
