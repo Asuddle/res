200d2cb254574d42dd423fa8c2a2cabf0648b732
ATsuddle <ahmad.suddle@lintelfs.com>
2022-11-12 19:37:07 +0500

    images gallery

--- components/Gallery/index.js
+++ components/Gallery/index.js
@@ -1,7 +1,16 @@
 import 'aos/dist/aos.css';
+import 'react-image-lightbox/style.css';
 
-import { Col, Row } from 'reactstrap';
-import React, { useEffect } from 'react';
+import {
+	Button,
+	Col,
+	Modal,
+	ModalBody,
+	ModalFooter,
+	ModalHeader,
+	Row,
+} from 'reactstrap';
+import React, { useEffect, useState } from 'react';
 import {
 	faCalendar,
 	faLocation,
@@ -11,16 +20,53 @@ import {
 import AOS from 'aos';
 import ContainerComponent from '../container';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import GalleryModal from './modal';
 import Header from '../Header';
 import HeadingComponent from '../Heading';
 import Image from 'next/image';
+import Lightbox from 'react-image-lightbox';
 import TabsComponent from '../tabs';
 import styles from '../../styles/gallery.module.scss';
+
+const galleryArr = [
+	{
+		name: 'Launch Party',
+		imageCount: 6,
+		date: '14th september 2021',
+		folder: '1',
+	},
+	{
+		name: 'Past life regression therapy',
+		imageCount: 25,
+		date: '22nd march 2022',
+		folder: '2',
+	},
+	{
+		name: 'Second Party',
+		imageCount: 25,
+		date: '16th october 2021',
+		folder: '3',
+	},
+	{
+		name: 'Third Party',
+		imageCount: 30,
+		date: '17th january 2022',
+		folder: '4',
+	},
+];
 
 export default function GalleryMainComponent() {
 	useEffect(() => {
 		AOS.init();
 	}, []);
+	const [data, setData] = useState({});
+	const toggle = () => {
+		setData({});
+	};
+	const getData = (item) => {
+		setData(item);
+	};
+
 	return (
 		<div className={styles.galleryWrapper}>
 			<ContainerComponent>
@@ -33,9 +79,9 @@ export default function GalleryMainComponent() {
 				<br />
 				<br />
 				<Row className='text-center'>
-					{[1, 2, 3, 4].map((item, idx) => (
-						<Col key={item} lg={4} md={6} sm={6} xs={6} data-aos='fade-up'>
-							<div className={styles.cardWrapper}>
+					{galleryArr.map((item, idx) => (
+						<Col key={item.name} lg={4} md={6} sm={6} xs={6} data-aos='fade-up'>
+							<div className={styles.cardWrapper} onClick={() => getData(item)}>
 								<Image
 									className='img-fluid'
 									src={`/home/Gallery${idx + 1}.jpg`}
@@ -46,7 +92,7 @@ export default function GalleryMainComponent() {
 									// objectFit='contain'
 								/>
 								<div className={styles.dataWrapper}>
-									<p className={styles.eventName}>Event Name here</p>
+									<p className={styles.eventName}>{item.name}</p>
 									<p className={styles.eventCaption}>
 										lorem ipsum dsad asd asd as das das das d as das d dasd
 										asdsa das das d asd ad as d
@@ -58,16 +104,15 @@ export default function GalleryMainComponent() {
 										</p>
 										<p className={styles.calendar}>
 											<FontAwesomeIcon icon={faCalendar} />
-											<span>20 Oct 2022</span>
+											<span>{item.date}</span>
 										</p>
 									</div>
 								</div>
 							</div>
 							<br />
-							{/* <br />
-							<br /> */}
 						</Col>
 					))}
+					<GalleryModal toggle={toggle} data={data} />
 				</Row>
 			</ContainerComponent>
 		</div>
