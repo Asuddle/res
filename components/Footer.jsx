ace36dfd9ff4b57911890452c661a41da1919f27
ATsuddle <ahmad.suddle@lintelfs.com>
2022-11-12 12:45:24 +0500

    test

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -73,7 +73,12 @@ export default function Footer() {
 					<div className='container py-5'>
 						<Row>
 							<Col lg={5} md={6} className={styles.footerDescriptionWrapper}>
-								<Image src='/zes2.png' alt='logo' width={100} height={100} />
+								<Image
+									src='/service-5.jpg'
+									alt='logo'
+									width={100}
+									height={100}
+								/>
 								<br />
 								<br />
 								<p className={styles.footerDescription}>
