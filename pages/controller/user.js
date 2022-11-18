bcce730c589f18245b663677feaae6754c16b1d2
ATsuddle <ahmad.suddle@lintelfs.com>
2022-11-19 00:15:47 +0500

    testtt

--- pages/controller/user.js
+++ pages/controller/user.js
@@ -1,25 +1,36 @@
-export const createUser = async (req, res) => {
-	try {
-		let { email, password, firstName } = req.body;
-		const existingUser = await User.findOne({ email });
-		if (existingUser) {
-			return res.send({ success: false, message: 'Email already exists!' });
-		}
-		const hash = await bcrypt.hash(password, 10);
-		req.body.password = hash;
-		console.log('hash', password);
-		const user = await User.create(req.body);
-		res.status(201).json({ success: true, data: user });
-	} catch (error) {
-		res.status(400).json({ success: false, error: error });
-	}
-};
+import PropTypes from 'prop-types';
+import React from 'react';
 
-export const getUser = async (req, res) => {
-	try {
-		const users = await User.find({});
-		res.status(200).json({ success: true, data: users });
-	} catch (error) {
-		res.status(400).json({ success: false });
-	}
-};
+function User(props) {
+	return <div>user</div>;
+}
+
+User.propTypes = {};
+
+export default User;
+
+// export const createUser = async (req, res) => {
+// 	try {
+// 		let { email, password, firstName } = req.body;
+// 		const existingUser = await User.findOne({ email });
+// 		if (existingUser) {
+// 			return res.send({ success: false, message: 'Email already exists!' });
+// 		}
+// 		const hash = await bcrypt.hash(password, 10);
+// 		req.body.password = hash;
+// 		console.log('hash', password);
+// 		const user = await User.create(req.body);
+// 		res.status(201).json({ success: true, data: user });
+// 	} catch (error) {
+// 		res.status(400).json({ success: false, error: error });
+// 	}
+// };
+
+// export const getUser = async (req, res) => {
+// 	try {
+// 		const users = await User.find({});
+// 		res.status(200).json({ success: true, data: users });
+// 	} catch (error) {
+// 		res.status(400).json({ success: false });
+// 	}
+// };
