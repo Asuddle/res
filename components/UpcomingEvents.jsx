122440c790f9c1fc86cd96ff8708d6ff9e888fb4
ATsuddle <ahmad.suddle@lintelfs.com>
2022-11-19 04:11:38 +0500

    smallfix

--- components/UpcomingEvents.jsx
+++ components/UpcomingEvents.jsx
@@ -1,5 +1,7 @@
 import { Col, Row } from 'reactstrap';
 
+import EventCard from './WhatNext/eventCard';
+import GalleryCard from './Gallery/GalleryCard';
 import HeadingComponent from './Heading';
 import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
@@ -18,35 +20,9 @@ export default function UpcomingEvents() {
 					subBoldHeading='Awesome Events'
 				/>
 				<Row className={styles.eventImage} data-aos='fade-up'>
-					{[1, 2, 3].map((item) => (
-						<Col key={item} xs={12} sm={12} className={styles.imageWrapper}>
-							<Image
-								src={`/service-${item}.jpg`}
-								layout='fill'
-								alt='Brand logo'
-							/>
-
-							<p className={styles.eventDate}>
-								15 <br /> <span>NOV</span>
-							</p>
-
-							<div className={styles.eventInfo}>
-								<p className={styles.heading}>Event Name Here</p>
-								<p className={styles.price}>3000 AED</p>
-								<br />
-								<p className={styles.venue}>
-									<i className='fa-solid fa-location-dot'></i> Venue Here
-								</p>
-								<p className={styles.venue}>
-									<i className='fa-solid fa-user'></i> Ladies & Gents
-								</p>
-								<br />
-								<div style={{ display: 'inline-flex' }}>
-									<button className={styles.bookNow}>BOOK NOW</button>
-									<p className={styles.moreDetail}>See Details</p>
-								</div>
-								<div className={styles.divider}></div>
-							</div>
+					{[1, 2, 3].map((item, idx) => (
+						<Col md={4} key={idx}>
+							<GalleryCard key={idx} item={item} idx={idx} />
 						</Col>
 					))}
 				</Row>
