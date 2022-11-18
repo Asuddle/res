ce56228e8faa996b0856c5e83893b64ef17bbf03
ATsuddle <ahmad.suddle@lintelfs.com>
2022-11-19 00:20:06 +0500

    check

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,48 +1,48 @@
-import User from '../../../models/User';
-import bcrypt from 'bcryptjs';
-import dbConnect from '../../../util/mongodb';
-import jwt from 'jsonwebtoken';
+// import User from '../../../models/User';
+// import bcrypt from 'bcryptjs';
+// import dbConnect from '../../../util/mongodb';
+// import jwt from 'jsonwebtoken';
 
-export default async function handler(req, res) {
-	const { method } = req;
+// export default async function handler(req, res) {
+// 	const { method } = req;
 
-	await dbConnect();
+// 	await dbConnect();
 
-	switch (method) {
-		case 'POST':
-			try {
-				let { email, password } = req.body;
-				const existingUser = await User.findOne({ email });
+// 	switch (method) {
+// 		case 'POST':
+// 			try {
+// 				let { email, password } = req.body;
+// 				const existingUser = await User.findOne({ email });
 
-				if (
-					!(existingUser && bcrypt.compareSync(password, existingUser.password))
-				) {
-					throw 'Username or password is incorrect';
-				}
+// 				if (
+// 					!(existingUser && bcrypt.compareSync(password, existingUser.password))
+// 				) {
+// 					throw 'Username or password is incorrect';
+// 				}
 
-				if (!existingUser.isVerified) {
-					return res.status(200).json({
-						id: existingUser.id,
-						message: 'Please wait to be verified by the admin',
-					});
-				}
+// 				if (!existingUser.isVerified) {
+// 					return res.status(200).json({
+// 						id: existingUser.id,
+// 						message: 'Please wait to be verified by the admin',
+// 					});
+// 				}
 
-				const token = jwt.sign({ sub: existingUser.id }, 'newsecret', {
-					expiresIn: '7d',
-				});
+// 				const token = jwt.sign({ sub: existingUser.id }, 'newsecret', {
+// 					expiresIn: '7d',
+// 				});
 
-				return res.status(200).json({
-					id: existingUser.id,
-					firstName: existingUser.firstName,
-					lastName: existingUser.lastName,
-					token,
-				});
-			} catch (error) {
-				res.status(400).json({ success: false, error: error });
-			}
-			break;
-		default:
-			res.status(400).json({ success: false });
-			break;
-	}
-}
+// 				return res.status(200).json({
+// 					id: existingUser.id,
+// 					firstName: existingUser.firstName,
+// 					lastName: existingUser.lastName,
+// 					token,
+// 				});
+// 			} catch (error) {
+// 				res.status(400).json({ success: false, error: error });
+// 			}
+// 			break;
+// 		default:
+// 			res.status(400).json({ success: false });
+// 			break;
+// 	}
+// }
--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,40 +1,40 @@
-// api/users.js
+// // api/users.js
 
-import User from '../../../models/User';
-import bcrypt from 'bcryptjs';
-import dbConnect from '../../../util/mongodb';
+// import User from '../../../models/User';
+// import bcrypt from 'bcryptjs';
+// import dbConnect from '../../../util/mongodb';
 
-export default async function handler(req, res) {
-	const { method } = req;
+// export default async function handler(req, res) {
+// 	const { method } = req;
 
-	await dbConnect();
+// 	await dbConnect();
 
-	switch (method) {
-		case 'GET':
-			try {
-				const users = await User.find({});
-				res.status(200).json({ success: true, data: users });
-			} catch (error) {
-				res.status(400).json({ success: false });
-			}
-			break;
-		case 'POST':
-			try {
-				let { email, password, firstName } = req.body;
-				const existingUser = await User.findOne({ email });
-				if (existingUser) {
-					return res.send({ success: false, message: 'Email already exists!' });
-				}
-				const hash = await bcrypt.hash(password, 10);
-				req.body.password = hash;
-				const user = await User.create(req.body);
-				res.status(201).json({ success: true, data: user });
-			} catch (error) {
-				res.status(400).json({ success: false, error: error });
-			}
-			break;
-		default:
-			res.status(400).json({ success: false });
-			break;
-	}
-}
+// 	switch (method) {
+// 		case 'GET':
+// 			try {
+// 				const users = await User.find({});
+// 				res.status(200).json({ success: true, data: users });
+// 			} catch (error) {
+// 				res.status(400).json({ success: false });
+// 			}
+// 			break;
+// 		case 'POST':
+// 			try {
+// 				let { email, password, firstName } = req.body;
+// 				const existingUser = await User.findOne({ email });
+// 				if (existingUser) {
+// 					return res.send({ success: false, message: 'Email already exists!' });
+// 				}
+// 				const hash = await bcrypt.hash(password, 10);
+// 				req.body.password = hash;
+// 				const user = await User.create(req.body);
+// 				res.status(201).json({ success: true, data: user });
+// 			} catch (error) {
+// 				res.status(400).json({ success: false, error: error });
+// 			}
+// 			break;
+// 		default:
+// 			res.status(400).json({ success: false });
+// 			break;
+// 	}
+// }
