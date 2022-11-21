63b1b104b445f4357526617a644db70311aed111
ATsuddle <ahmad.suddle@lintelfs.com>
2022-11-21 19:03:29 +0500

    testing logo

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -45,7 +45,7 @@ function Example() {
 					<nav className='navbar navbar-expand-lg'>
 						<Image
 							className='navbar-brand'
-							src='/ZeS2-black.jpg'
+							src='/zes2-black.jpg'
 							alt='logo'
 							width={100}
 							height={100}
