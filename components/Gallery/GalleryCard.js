673c92a78b732154d82233881be506c01af46faf
ATsuddle <ahmad.suddle@lintelfs.com>
2022-11-21 22:00:03 +0500

    date

--- components/Gallery/GalleryCard.js
+++ components/Gallery/GalleryCard.js
@@ -5,6 +5,32 @@ import Image from 'next/image';
 import homeStyles from '../../styles/Home.module.scss';
 import styles from '../../styles/gallery.module.scss';
 
+const galleryArr = [
+	{
+		name: 'Launch Party',
+		imageCount: 6,
+		date: '14th september 2021',
+		folder: '1',
+	},
+	{
+		name: 'Past life regression therapy',
+		imageCount: 25,
+		date: '22nd march 2022',
+		folder: '2',
+	},
+	{
+		name: 'Second Party',
+		imageCount: 25,
+		date: '16th october 2021',
+		folder: '3',
+	},
+	{
+		name: 'Third Party',
+		imageCount: 30,
+		date: '17th january 2022',
+		folder: '4',
+	},
+];
 export default function GalleryCard({ idx, item }) {
 	return (
 		<div className={styles.cardWrapper}>
@@ -19,8 +45,11 @@ export default function GalleryCard({ idx, item }) {
 			/>
 			<div className={styles.dataWrapper}>
 				<p className={styles.eventDate}>
-					<span>1 Dec</span>
+					<span>01</span>
+					<br />
+					Dec
 				</p>
+				<p className={styles.eventName}></p>
 				{/* <p className={styles.eventDate}>{item.name}</p> */}
 				<p className={styles.eventCaption}>
 					lorem ipsum dsad asd asd as das das das d as das d dasd asdsa das das
